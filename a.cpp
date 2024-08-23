212f9428325eab44302d020287a88a14a3448d60
Ashish <theashish752@gmail.com>
2024-08-23 14:18:28 +0530

    added

---
+++ a.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    stirng str = "Hi This is the code";
+    for(auto it: str){
+        cout<<it<<" "; 
+    }cout<<endl;
+    return 0;
+}
